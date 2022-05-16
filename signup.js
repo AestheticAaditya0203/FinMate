77fac5122ba29f2210e912792d94ddfb18f42f3f
AestheticAaditya0203 <103627563+AestheticAaditya0203@users.noreply.github.com>
2022-05-16 08:40:59 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,34 @@
+const var1 = doument.querySelector('#Pass').value;
+//const var2 = doument.querySelector('#Pass1').value;
+
+if(var1 === "") {  
+   document.querySelector("Pass").innerHTML = "**Fill the password please!"; 
+} 
+/* else if(vars1.length<8)
+{
+    
+}
+
+
+function verifyPassword() {  
+    var pw = document.getElementById("pswd").value;  
+    //check empty password field  
+    if(pw == "") {  
+       document.getElementById("message").innerHTML = "**Fill the password please!";  
+       return false;  
+    }  
+     
+   //minimum password length validation  
+    if(pw.length < 8) {  
+       document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+       return false;  
+    }  
+    
+  //maximum length of password validation  
+    if(pw.length > 15) {  
+       document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+       return false;  
+    } else {  
+       alert("Password is correct");  
+    }  
+  }   */
